9b6a2e01b9fbe7cd841d3d41fac98b5b3d4ba7d8
Valerian <93781706+vaanrile@users.noreply.github.com>
2024-10-07 12:11:04 +0200

    Sons interactifs

--- Assets/Src/Scripts/S_Kickable.cs
+++ Assets/Src/Scripts/S_Kickable.cs
@@ -11,6 +11,8 @@ public class S_Kickable : S_AbsInteractive
     private void Awake()
     {
         _audioSource = gameObject.AddComponent<AudioSource>();
+        _audioSource.spatialBlend = 1;
+        
     }
 
     public override void Kicked()
@@ -28,6 +30,14 @@ public class S_Kickable : S_AbsInteractive
         {
             _audioSource.PlayOneShot(AudioManager.instance.hitArray[Random.Range(0, AudioManager.instance.hitArray.Length - 1)]);
             _audioSource.PlayOneShot(AudioManager.instance.findAudioClip(audioName));
+        }
+        _audioSource.pitch = Random.Range(1.5f,3);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Kicked();
         }
     }
 }
